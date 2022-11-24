629bf3ecdafdc266df8ddba7b0b48bdf3ab8203b
Lisa Allen <79679706+CrypticMango@users.noreply.github.com>
2022-11-24 06:03:02 -0600

    close menu function

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const theBurger = document.getElementById("the-burger");
 const menu = document.getElementById("top-menu");
 
-theBurger.addEventListener("click", toggleMenu);
+const about = document.getElementById("about");
+const services = document.getElementById("services");
+const projects = document.getElementById("projects");
+const contact = document.getElementById("contact");
 
-function toggleMenu() {
+theBurger.addEventListener("click", openMenu);
+
+function openMenu() {
 	menu.style.display = "block";
+}
+
+about.addEventListener("click", closeMenu);
+services.addEventListener("click", closeMenu);
+projects.addEventListener("click", closeMenu);
+contact.addEventListener("click", closeMenu);
+
+function closeMenu() {
+	menu.style.display = "none";
 }
